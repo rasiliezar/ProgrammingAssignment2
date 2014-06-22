52025496c82af6a73bc50450c392033b1ba5ec30
Alfredo Siliezar <rasiliezar@gmail.com>
2014-06-22 04:05:21 -0600

    tarea completa

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Tarea 2 de R Programming
+## Ejecuta una sola vez la creación de una matriz inversa
 
-## Write a short comment describing this function
+## Definir sets y gets para las funciones
 
 makeCacheMatrix <- function(x = matrix()) {
+	
+	miv <- NULL
 
+	set <- function(y) {
+      	x <<- y
+        	miv <<- NULL
+    	}
+    
+    	get <- function() x
+
+    	setmiv <- function(inverse) miv <<- inverse
+    	
+    	getmiv <- function() miv
+
+    	list(set = set, get = get, setmiv = setmiv, getmiv = getmiv)
 }
 
-
-## Write a short comment describing this function
+## Regresa una matriz que es la inversa de x
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    	miv <- x$getmiv()
+
+     	if (!is.null(miv)) {
+        	message("Obteniendo data de caché")
+        	return(miv)
+    	}
+
+    	data <- x$get()
+    	miv <- solve(data, ...)
+
+    	x$setmiv(miv)
+
+    	miv
 }
